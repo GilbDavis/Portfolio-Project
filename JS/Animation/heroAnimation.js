c846f9d5d640e946ac425a08d089ab89be3630a2
Gilberto <peter01@hotmail.es>
2020-04-03 01:38:57 -0500

    Changed the hero image, added some palette colors for testing, added JS+Css to handle the hero typewritter effect

---
+++ JS/Animation/heroAnimation.js
@@ -0,0 +1,35 @@
+let aText = new Array(
+  "Hello there!", "I'm <span id='name-highlight'>Gilberto Davis</span>, a full-stack Web &ltDeveloper/&gt."
+);
+let iSpeed = 60; // time delay of print out
+let iIndex = 0; // start printing array at this posision
+let iArrLength = aText[0].length; // the length of the text array
+let iScrollAt = 20; // start scrolling up at this many lines
+
+let iTextPos = 0; // initialise text position
+let sContents = ''; // initialise contents variable
+let iRow; // initialise current row
+
+function typewriter() {
+  sContents = ' ';
+  iRow = Math.max(0, iIndex - iScrollAt);
+  let destination = document.getElementById("typeWritter");
+
+  while (iRow < iIndex) {
+    sContents += aText[iRow++] + '<br />';
+  }
+  destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos);
+  if (iTextPos++ == iArrLength) {
+    iTextPos = 0;
+    iIndex++;
+    if (iIndex != aText.length) {
+      iArrLength = aText[iIndex].length;
+      setTimeout("typewriter()", 500);
+    }
+  } else {
+    setTimeout("typewriter()", iSpeed);
+  }
+}
+
+
+typewriter();
