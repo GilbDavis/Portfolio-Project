feeeeabf763c95222d06d4d1ee858d58a28b415d
Gilberto <peter01@hotmail.es>
2020-04-19 12:38:54 -0500

    Changes before trying heading

--- JS/Animation/heroAnimation.js
+++ JS/Animation/heroAnimation.js
@@ -1,7 +1,7 @@
 let aText = new Array(
   "Hello there!", "I'm <span id='name-highlight'>Gilberto Davis</span>, a full-stack Web &ltDeveloper/&gt."
 );
-let iSpeed = 60; // time delay of print out
+let iSpeed = 45; // time delay of print out
 let iIndex = 0; // start printing array at this posision
 let iArrLength = aText[0].length; // the length of the text array
 let iScrollAt = 20; // start scrolling up at this many lines
