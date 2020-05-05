b65f4e7930a9305d5a5dddf28749b05f5fc5a9b3
Gilberto <peter01@hotmail.es>
2020-05-05 17:37:48 -0500

    Added contact section desktop view styles, removed social media icons and location info

--- src/JS/main.js
+++ src/JS/main.js
@@ -3,9 +3,11 @@ import changeActiveMenu from './Animation/changeActiveMenu';
 import headerMenuBackgroundChange from './Animation/headerBackgroundChange';
 import toggleHamburger from './Animation/toggleHamburger';
 import lazyLoading from './utility/lazyLoading';
+import netlifyFormSendEmail from './utility/sendEmail';
 
 smoothScrolling();
 changeActiveMenu();
 toggleHamburger();
 lazyLoading();
+netlifyFormSendEmail();
 window.onscroll = function () { headerMenuBackgroundChange() };
---
+++ src/JS/utility/sendEmail.js
@@ -0,0 +1,24 @@
+export default function netlifyFormSendEmail() {
+
+  let form = document.querySelector("#EmailSection form");
+  let successParagraph = document.querySelector('#sendMessage');
+
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    fetch(form.getAttribute('actions'), {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      },
+      body: new URLSearchParams(formData.toString())
+    })
+      .then(res => {
+        if (res) {
+          successParagraph.textContent += "Thank you for your message. It has been sent."
+        }
+      })
+  });
+}
